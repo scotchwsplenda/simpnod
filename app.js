d1cddcf599be797c1b60f4cd98311f6a27016158
scotchwsplenda <lockwood.oliver@gmail.com>
2023-12-15 10:34:31 -0800

    initty

---
+++ app.js
@@ -0,0 +1,12 @@
+// index.js
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, Azure Static Web Apps!');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
